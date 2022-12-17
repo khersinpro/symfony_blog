15b44c6692fb3bc8d6d0994c9547a4659397ce01
khersinpro <khersinpro@gmail.com>
2022-12-17 17:24:10 +0100

    add security , session with signup and signin, remember me token

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/signup', name: 'app_signup')]
+    public function signup(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user =  new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('app_signin');
+        }
+
+        return $this->render('security/signup.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/signin', name: 'app_signin')]
+    public function signin(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/signin.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+    
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: 'email', message: "L'email est déjà utilisé.")]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[Assert\NotBlank(message: "Ce champ ne peux pas être vide")]
+    #[Assert\Email(message: 'L\'email {{ value }} n\'est pas un email.')]
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+    
+    #[ORM\Column]
+    private array $roles = [];
+    
+    /**
+     * @var string The hashed password
+     */
+    #[Assert\NotBlank(message: "Ce champ ne peux pas être vide")]
+    #[Assert\Length(
+        min: 6, max: 25,
+        minMessage: 'Le mot de passe doit contenir {{ limit }} caractères minimum.',
+        maxMessage: 'Le mot de passe ne doit pas dépasser {{ limit }} caractères.'
+    )]
+    #[ORM\Column]
+    private ?string $password = null;
+    
+    #[Assert\NotBlank(message: "Ce champ ne peux pas être vide")]
+    #[Assert\Length(
+        min: 3, max: 25,
+        minMessage: 'Le pseudo doit contenir {{ limit }} caractères minimum.',
+        maxMessage: 'Le pseudo ne doit pas dépasser {{ limit }} caractères.'
+    )]
+    #[ORM\Column(length: 50)]
+    private ?string $nickname = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getNickname(): ?string
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname(string $nickname): self
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+}
